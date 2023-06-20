1c2b2c746031a57217f9157af4bd04ce4786a58e
Surya Ashish <ashishsurya126@gmail.com>
2023-06-21 01:34:34 +0530

    animations taken care

--- src/components/forms/NewJobForm.tsx
+++ src/components/forms/NewJobForm.tsx
@@ -1,9 +1,13 @@
 import { useAppDispatch } from '@/redux/hooks';
 import { set } from '@/redux/slices/dialogs/newJobDialogSlice';
-import React from 'react';
+import React, { useState } from 'react';
+import * as Select from '@radix-ui/react-select';
+import { FiChevronDown } from 'react-icons/fi';
+import { motion } from 'framer-motion';
 
 const NewJobForm = () => {
   const dispatch = useAppDispatch();
+
   return (
     <form
       className='flex flex-col gap-[10px]'
@@ -21,9 +25,36 @@ const NewJobForm = () => {
         className='focus:outline-none  h-full rounded-lg px-[20px] py-[12px] border w-full'
         placeholder='Job Url'
       />
-      <select className='focus:outline-none  h-full rounded-lg px-[20px] py-[12px] border w-full'>
-        <option value='saved'>Saved</option>
-      </select>
+
+      <Select.Root required>
+        <Select.Trigger className='flex justify-between px-[20px] py-[12px] border rounded-lg items-center  text-[#5a5a5a]'>
+          <Select.Value placeholder='Select status' />
+          <Select.Icon>
+            <FiChevronDown />
+          </Select.Icon>
+        </Select.Trigger>
+
+        <Select.Portal>
+          <Select.Content asChild>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className='bg-white top-14 left-0 absolute  p-[10px] w-4/5 shadow-lg rounded-lg border'
+            >
+              <Select.Viewport>
+                <Select.Group className='flex flex-col gap-[10px] items-start'>
+                  <CustomSelectItem textValue='Saved' value='saved' />
+                  <CustomSelectItem
+                    textValue='Interviewing'
+                    value='interviewing'
+                  />
+                </Select.Group>
+              </Select.Viewport>
+            </motion.div>
+          </Select.Content>
+        </Select.Portal>
+      </Select.Root>
 
       <div className='flex mt-10 gap-[10px]'>
         <button
@@ -37,6 +68,25 @@ const NewJobForm = () => {
         </button>
       </div>
     </form>
+  );
+};
+
+const CustomSelectItem = ({
+  value,
+  textValue,
+}: {
+  value: string;
+  textValue: string;
+}) => {
+  return (
+    <Select.Item
+      className={
+        'cursor-pointer w-full p-[10px] data-[state=unchecked]:hover:bg-[#f5f5f5] data-[state=checked]:bg-purple-600 data-[state=checked]:text-white rounded-lg duration-200; text-sm'
+      }
+      value={value}
+    >
+      <Select.ItemText>{textValue}</Select.ItemText>
+    </Select.Item>
   );
 };
 
